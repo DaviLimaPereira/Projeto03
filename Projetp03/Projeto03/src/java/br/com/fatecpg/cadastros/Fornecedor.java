74f9c0ea51cbe5ff3c6720c20576d27d9a78e255
LeticiaCury <leticiacury2009@hotmail.com>
2019-03-31 20:12:28 -0300

    Commit Leticia

--- Projetp03/Projeto03/src/java/br/com/fatecpg/cadastros/Fornecedor.java
+++ Projetp03/Projeto03/src/java/br/com/fatecpg/cadastros/Fornecedor.java
@@ -10,5 +10,59 @@ package br.com.fatecpg.cadastros;
  * @author davil
  */
 public class Fornecedor {
+    private String nome;
+    private String razaosocial;
+    private String cnpj;
+    private String email;
+    private String telefone;
+    private String endereco;
     
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getRazaosocial() {
+        return razaosocial;
+    }
+
+    public void setRazaosocial(String razaosocial) {
+        this.razaosocial = razaosocial;
+    }
+    
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
